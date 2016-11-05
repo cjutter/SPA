0b2a140cee65603bc0dbdc299251fc0ad832543a
Charles Utter <Charles Utter>
2016-11-05 18:58:36 -0400

    added repo

--- WebServices/WebServices/Models/Reservation.cs
+++ WebServices/WebServices/Models/Reservation.cs
@@ -7,5 +7,8 @@ namespace WebServices.Models
 {
     public class Reservation
     {
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string Location { get; set; }        
     }
 }
